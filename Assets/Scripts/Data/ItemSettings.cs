d8496cdb7162176d2875174ad29e3d2bf788ce58
DmitryDemenkov <demon.krut2002@mail.ru>
2025-11-04 19:17:14 +0300

    [Change] ItemSO item type from GameObject to ItemView

--- Assets/Scripts/Data/ItemSettings.cs
+++ Assets/Scripts/Data/ItemSettings.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 public class ItemSettings
 {
     public int itemID;
-    public GameObject item;
+    public ItemView item;
     public Row[] block;
+
+    public int[,] GetStructure()
+    {
+        int[,] structure = new int[block.Length, block[0].block_m.Length];
+        for(int i = 0; i < block.Length; i++)
+        {
+            for (int j = 0; j < block[i].block_m.Length; j++)
+            {
+                structure[i, j] = block[i].block_m[j];
+            }
+        }
+        return structure;
+    }
 }
 
 [System.Serializable]
