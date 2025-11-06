87db506e45b5389f5caac1c1c21c5ac4fa061031
Klim Ananenko <aklimv@mail.ru>
2025-11-07 00:35:07 +0300

    [Refactoring] In CellView implemented Enum

--- Assets/Scripts/View/CellView.cs
+++ Assets/Scripts/View/CellView.cs
@@ -1,13 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum CellState :int
+{
+    NORMAL = 0,
+    VALID = 1,
+    INVALID = 2
+}
+
 public class CellView : MonoBehaviour
 {
-    [SerializeField] private Image backgroundImage;
+    [SerializeField] private Image backgroundImage; 
 
-    private Color normalColor = Color.white;
-    private Color validColor = Color.green;
-    private Color invalidColor = Color.red;
+    private Dictionary<CellState, Color> stateToColor = new()
+    {
+        { CellState.NORMAL, Color.white },
+        { CellState.VALID, Color.green },
+        { CellState.INVALID, Color.red }
+    };
 
     void Awake()
     {
@@ -15,39 +26,20 @@ public class CellView : MonoBehaviour
         {
             backgroundImage = GetComponent<Image>();
         }
-
-        if (backgroundImage != null)
-        {
-            normalColor = backgroundImage.color;
-        }
     }
 
     public void SetHighlight(int state)
     {
         if (backgroundImage == null) return;
 
-        switch (state)
-        {
-            case 0:
-                backgroundImage.color = normalColor;
-                break;
-            case 1:
-                backgroundImage.color = validColor;
-                break;
-            case 2:
-                backgroundImage.color = invalidColor;
-                break;
-            default:
-                backgroundImage.color = normalColor;
-                break;
-        }
+        backgroundImage.color = stateToColor[(CellState)state];
     }
 
     public void ResetHighlight()
     {
         if (backgroundImage != null)
         {
-            backgroundImage.color = normalColor;
+            backgroundImage.color = stateToColor[CellState.NORMAL];
         }
     }
 }
