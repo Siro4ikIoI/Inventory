61c69b5d213d80c301ea185af9f80bc50868ba99
DmitryDemenkov <demon.krut2002@mail.ru>
2025-11-10 20:58:33 +0300

    [Add] Item rotation model

---
+++ Assets/Runtime/Data/Direction.cs
@@ -0,0 +1,7 @@
+public enum Direction : int
+{
+    N = 0,
+    E = 1,
+    S = 2,
+    W = 3
+}
--- Assets/Runtime/Model/Item.cs
+++ Assets/Runtime/Model/Item.cs
@@ -4,14 +4,37 @@ public class Item : IMatrix
 
     public int Id { get; private set; }
 
+    private Direction _rotation;
+
     public Item(int id, int[,] blocks)
     {
         Id = id;
         _blocks = new(blocks);
+        _rotation = Direction.N;
     }
 
     public Matrix ToMatrix()
     {
         return _blocks;
     }
+
+    public void Rotate()
+    {
+        _blocks = _blocks.Rotate();
+        _rotation++;
+    }
+
+    public void SetRotation(Direction direction)
+    {
+        int rotationAmount = direction - _rotation;
+        if (rotationAmount < 0)
+            rotationAmount += 4;
+
+        for (int i = 0; i < rotationAmount; i++)
+        {
+            Rotate();
+        }
+    }
+
+    public Direction GetRotation() => _rotation;
 }
