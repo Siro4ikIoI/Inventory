d8d7625a428caec34b497a90db6d303506829e89
Klim Ananenko <aklimv@mail.ru>
2025-11-12 16:35:52 +0300

    [Refactoring] CellWiew
    - removed GetComponent
    - removed null checks

--- Assets/Runtime/View/CellView.cs
+++ Assets/Runtime/View/CellView.cs
@@ -2,44 +2,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum CellState :int
-{
-    NORMAL = 0,
-    VALID = 1,
-    INVALID = 2
-}
-
 public class CellView : MonoBehaviour
 {
-    [SerializeField] private Image backgroundImage; 
+    [SerializeField] private Image _backgroundImage;
 
-    private Dictionary<CellState, Color> stateToColor = new()
+    private Dictionary<CellViewState, Color> stateToColor = new()
     {
-        { CellState.NORMAL, Color.white },
-        { CellState.VALID, Color.green },
-        { CellState.INVALID, Color.red }
+        { CellViewState.NORMAL, Color.white },
+        { CellViewState.VALID, Color.green },
+        { CellViewState.INVALID, Color.red }
     };
-
-    void Awake()
-    {
-        if (backgroundImage == null)
-        {
-            backgroundImage = GetComponent<Image>();
-        }
-    }
 
     public void SetHighlight(int state)
     {
-        if (backgroundImage == null) return;
-
-        backgroundImage.color = stateToColor[(CellState)state];
+        _backgroundImage.color = stateToColor[(CellViewState)state];
     }
 
     public void ResetHighlight()
     {
-        if (backgroundImage != null)
-        {
-            backgroundImage.color = stateToColor[CellState.NORMAL];
-        }
+        _backgroundImage.color = stateToColor[CellViewState.NORMAL];
     }
 }
