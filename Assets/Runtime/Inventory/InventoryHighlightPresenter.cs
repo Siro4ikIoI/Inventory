10f7cd6d2d9e96453165a7fc5102ae8a35868545
DmitryDemenkov <demon.krut2002@mail.ru>
2025-11-14 14:30:09 +0300

    [Refactoring] Inventory highlight logic

---
+++ Assets/Runtime/Inventory/InventoryHighlightPresenter.cs
@@ -0,0 +1,82 @@
+public class InventoryHighlightPresenter
+{
+    private ModelCollection _modelCollection;
+    private CanvasView _canvasView;
+
+    private DragAndDropModel _dragAndDropModel;
+
+    public InventoryHighlightPresenter(ModelCollection modelCollection, CanvasView canvasView)
+    {
+        _modelCollection = modelCollection;
+        _canvasView = canvasView;
+    }
+
+    private void OnDragAndDropChanged(DragAndDropModel dragAndDropModel)
+    {
+        if (dragAndDropModel == null)
+        {
+            _dragAndDropModel.Item.Rotated -= OnItemRotated;
+            _dragAndDropModel.EndDraged -= OnEndDrag;
+            _dragAndDropModel.Draged -= OnDrag;
+
+            _dragAndDropModel = null;
+        }
+        else
+        {
+            _dragAndDropModel = dragAndDropModel;
+
+            _dragAndDropModel.Draged += OnDrag;
+            _dragAndDropModel.EndDraged += OnEndDrag;
+            _dragAndDropModel.Item.Rotated += OnItemRotated;
+        }
+    }
+
+    private void OnDrag()
+    {
+        HighlightInventoryCells();
+    }
+
+    private void OnEndDrag()
+    {
+        ResetAllHighlights();
+    }
+
+    private void OnItemRotated(Direction obj)
+    {
+        HighlightInventoryCells();
+    }
+
+    private void ResetAllHighlights()
+    {
+        Item emptyItem = new Item(-1, new int[1, 1] { { 0 } });
+        foreach (var inventory in _modelCollection.GetAllInventories())
+        {
+            inventory.CanAddItem(emptyItem, new Pair(0, 0));
+        }
+    }
+
+    private void HighlightInventoryCells()
+    {
+        InventoryType inventoryType = _dragAndDropModel.CurrentInventory;
+        if (inventoryType == InventoryType.NONE)
+        {
+            ResetAllHighlights();
+            return;
+        }
+
+        Inventory inventory = _modelCollection.GetInventory(inventoryType);
+        Pair itemPosition = _dragAndDropModel.CurrentPosition;
+
+        inventory.CanAddItem(_dragAndDropModel.Item, itemPosition);
+    }
+
+    public void Enable()
+    {
+        _modelCollection.ChangedDragAndDrop += OnDragAndDropChanged;
+    }
+
+    public void Disable()
+    {
+        _modelCollection.ChangedDragAndDrop -= OnDragAndDropChanged;
+    }
+}
