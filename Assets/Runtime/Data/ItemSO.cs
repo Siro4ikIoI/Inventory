19c2b03915b2475f56245f359cec7893ccad907e
DmitryDemenkov <demon.krut2002@mail.ru>
2025-11-16 16:06:56 +0300

    [Refactoring] ItemSO structure

--- Assets/Runtime/Data/ItemSO.cs
+++ Assets/Runtime/Data/ItemSO.cs
@@ -3,5 +3,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Items", menuName = "Game/Items")]
 public class ItemSO : ScriptableObject
 {
-    public ItemSettings[] items;
+    public ItemType type;
+    public ItemView item;
+    public Row[] row;
+
+    public int[,] GetStructure()
+    {
+        int[,] structure = new int[row.Length, row[0].col.Length];
+        for (int i = 0; i < row.Length; i++)
+        {
+            for (int j = 0; j < row[i].col.Length; j++)
+            {
+                structure[i, j] = row[i].col[j];
+            }
+        }
+        return structure;
+    }
+}
+
+[System.Serializable]
+public class Row
+{
+    public int[] col;
 }
