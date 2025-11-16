0b2c0e6eefe3df099b531bcdd7765c163a8b15f4
Klim Ananenko <aklimv@mail.ru>
2025-11-16 03:23:21 +0300

    [Refactoring] DragAndDrop

--- Assets/Runtime/DragAndDrop/DragAndDropPresenter.cs
+++ Assets/Runtime/DragAndDrop/DragAndDropPresenter.cs
@@ -24,6 +24,8 @@ public class DragAndDropPresenter
         if (_modelCollection.GetCurrentDragAndDrop() != null)
             return;
 
+        _dragAndDropModel.SetInventoryAndPosition(InventoryType.NONE, new Pair(-1, -1));
+
         Inventory sourceInventory = null;
         foreach (Inventory inventory in _modelCollection.GetAllInventories())
         {
@@ -49,20 +51,11 @@ public class DragAndDropPresenter
         if (_modelCollection.GetCurrentDragAndDrop() != _dragAndDropModel)
             return;
 
-        ItemView itemView = _canvasView.GetItemView(_dragAndDropModel.Item.Id);
-        itemView.SetPosition(itemView.GetPosition() + delta / _canvasView.Canvas.scaleFactor);
+        RectTransform rectTransform = (RectTransform)_dragAndDropView.transform;
+        rectTransform.anchoredPosition += delta / _canvasView.Canvas.scaleFactor;
 
-        _dragAndDropModel.CurrentInventory = GetInventoryAtScreenPosition(itemView.transform.position);
-        Pair itemPosition = new Pair(-1, -1);
-        if (_dragAndDropModel.CurrentInventory != InventoryType.NONE)
-        {
-            Inventory inventory = _modelCollection.GetInventory(_dragAndDropModel.CurrentInventory);
-            InventoryView inventoryView = _canvasView.GetInventoryView(_dragAndDropModel.CurrentInventory);
-            itemPosition = GetItemPosition(itemView.transform.position, inventory, inventoryView);
-        }
-
-        _dragAndDropModel.CurrentPosition = itemPosition;
-        _dragAndDropModel.Drag();
+        Vector3 worldPosition = rectTransform.position;
+        _dragAndDropModel.Drag(worldPosition.x, worldPosition.y);
     }
 
     private void OnEndDrag() 
--- Assets/Runtime/Inventory/InventoryPresenter.cs
+++ Assets/Runtime/Inventory/InventoryPresenter.cs
@@ -9,6 +9,8 @@ public class InventoryPresenter
     private ModelCollection _modelCollection;
     private CanvasView _canvasView;
 
+    private DragAndDropModel _dragAndDropModel;
+
     public InventoryPresenter(Inventory inventory, InventoryView inventoryView, 
                                 ModelCollection modelCollection, CanvasView canvasView)
     {
@@ -46,6 +48,8 @@ public class InventoryPresenter
         _inventory.ItemAdded += OnItemAdded;
         _inventory.ItemExtracted += OnItemExtracted;
         _inventory.CollisionWhenAdding += OnCollisionWhenAdding;
+
+        _modelCollection.ChangedDragAndDrop += OnDragAndDropChanged;
     }
 
     public void Disable()
@@ -53,5 +57,55 @@ public class InventoryPresenter
         _inventory.CollisionWhenAdding -= OnCollisionWhenAdding;
         _inventory.ItemExtracted -= OnItemExtracted;
         _inventory.ItemAdded -= OnItemAdded;
+
+        _modelCollection.ChangedDragAndDrop -= OnDragAndDropChanged;
+        if (_dragAndDropModel != null)
+        {
+            _dragAndDropModel.Draged -= OnDragged;
+            _dragAndDropModel = null;
+        }
+    }
+
+    private void OnDragAndDropChanged(DragAndDropModel dragAndDropModel)
+    {
+        if (_dragAndDropModel != null)
+        {
+            _dragAndDropModel.Draged -= OnDragged;
+        }
+
+        _dragAndDropModel = dragAndDropModel;
+
+        if (_dragAndDropModel != null)
+        {
+            _dragAndDropModel.Draged += OnDragged;
+        }
+    }
+
+    private void OnDragged(float x, float y)
+    {
+        Vector3 position = new Vector3(x, y, 0f);
+
+        if (!_inventoryView.IsPointInside(position))
+            return;
+
+        InventoryType inventoryType = InventoryType.NONE;
+        foreach (var pair in _canvasView.GetAllInventoryViews())
+        {
+            if (pair.Value == _inventoryView)
+            {
+                inventoryType = pair.Key;
+                break;
+            }
+        }
+
+        if (inventoryType == InventoryType.NONE)
+            return;
+
+        _inventoryView.GetTablePosition(position, out Vector2 tablePosition);
+        int col = (int)tablePosition.x;
+        int row = (int)(_inventory.Shape.Row - tablePosition.y - 1);
+        Pair cellPosition = new Pair(row, col);
+
+        _dragAndDropModel.SetInventoryAndPosition(inventoryType, cellPosition);
     }
 }
