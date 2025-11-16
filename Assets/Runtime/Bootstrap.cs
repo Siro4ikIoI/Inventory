b0d6824cd8cbac52e42ab89a9439a2098004c2e7
DmitryDemenkov <demon.krut2002@mail.ru>
2025-11-16 16:28:06 +0300

    [Refactoring] Bootstrap

--- Assets/Runtime/Bootstrap.cs
+++ Assets/Runtime/Bootstrap.cs
@@ -6,10 +6,17 @@ using UnityEngine;
 
 public class Bootstrap : MonoBehaviour
 {
-    [SerializeField] ItemSO itemSO;
-    [SerializeField] CanvasView canvasView;
-    [SerializeField] ItemViewGenerator ItemViewGenerator;
+    [Header("Inventories")]
+    [SerializeField] InventoryView inventory;
+    [SerializeField] InventoryView caseInventory;
+
+    [Header("Item Settings")]
+    [SerializeField] ItemSettings itemSettings;
+
+    [Header("Input")]
     [SerializeField] InputHandler input;
+
+    [Header("UI")]
     [SerializeField] ScoreView scoreView;
 
     private ModelCollection _modelCollection;
@@ -17,7 +24,6 @@ public class Bootstrap : MonoBehaviour
     private InventoryPresenter _inventoryPresenter;
     private InventoryPresenter _caseInventoryPresenter;
     private ItemGenerationPresenter _itemGenerationPresenter;
-    private DragAndDropCreator _dragAndDropCreator;
     private ItemRotationPresenter _itemRotationPresenter;
     private InventoryHighlightPresenter _inventoryHighlightPresenter;
     private ScorePresenter _scorePresenter;
@@ -26,35 +32,32 @@ public class Bootstrap : MonoBehaviour
     {
         _modelCollection = new ModelCollection();
 
-        _inventoryPresenter = CreateInventoryPresenter(InventoryType.INVENTARY, new Pair(5, 9));
-        _caseInventoryPresenter = CreateInventoryPresenter(InventoryType.CASE, new Pair(7, 4));
+        _inventoryPresenter = CreateInventoryPresenter(InventoryType.INVENTARY, new Pair(5, 9), inventory);
+        _caseInventoryPresenter = CreateInventoryPresenter(InventoryType.CASE, new Pair(7, 4), caseInventory);
 
-        ItemGenerator itemGenerator = new ItemGenerator(itemSO);
-        _itemGenerationPresenter = new ItemGenerationPresenter(itemGenerator, ItemViewGenerator, _modelCollection, canvasView);
+        ItemGenerator itemGenerator = new ItemGenerator(itemSettings);
+        _itemGenerationPresenter = new ItemGenerationPresenter(itemGenerator, _modelCollection);
 
-        _dragAndDropCreator = new DragAndDropCreator(_modelCollection, canvasView);
-        _itemRotationPresenter = new ItemRotationPresenter(input, _modelCollection, canvasView);
-        _inventoryHighlightPresenter = new InventoryHighlightPresenter(_modelCollection, canvasView);
-        _scorePresenter = new ScorePresenter(new GameState(), scoreView, _modelCollection, canvasView);
+        _itemRotationPresenter = new ItemRotationPresenter(input, _modelCollection);
+        _inventoryHighlightPresenter = new InventoryHighlightPresenter(_modelCollection);
+        _scorePresenter = new ScorePresenter(new GameState(), scoreView, _modelCollection);
 
         _inventoryPresenter.Enable();
         _caseInventoryPresenter.Enable();
         _inventoryHighlightPresenter.Enable();
-        _dragAndDropCreator.Enable();
         _itemRotationPresenter.Enable();
         _scorePresenter.Enable();
         _itemGenerationPresenter.Enable();
     }
 
-    private InventoryPresenter CreateInventoryPresenter(InventoryType inventoryType, Pair inventoryShape)
+    private InventoryPresenter CreateInventoryPresenter(InventoryType inventoryType, Pair inventoryShape, InventoryView inventoryView)
     {
         Inventory inventory = new Inventory(inventoryType, inventoryShape);
         _modelCollection.AddInventory(inventoryType, inventory);
-        
-        InventoryView inventoryView = canvasView.GetInventoryView(inventoryType);
+
         inventoryView.SetShape(inventory.Shape.Row, inventory.Shape.Col);
         
-        return new InventoryPresenter(inventory, inventoryView, _modelCollection, canvasView);
+        return new InventoryPresenter(inventory, inventoryView, _modelCollection, itemSettings);
     }
 
     private void OnDestroy()
@@ -62,7 +65,6 @@ public class Bootstrap : MonoBehaviour
         _itemGenerationPresenter.Disable();
         _scorePresenter.Disable();
         _itemRotationPresenter.Disable();
-        _dragAndDropCreator.Disable();
         _inventoryHighlightPresenter.Disable();
         _caseInventoryPresenter.Disable();
         _inventoryPresenter.Disable();
